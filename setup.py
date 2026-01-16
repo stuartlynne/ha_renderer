fd2ab71919341f8f254a86398acb27365c1a6023
stuart lynne <stuart.lynne@gmail.com>
2026-01-16 00:12:05 -0800

    interim checkin for docker build work

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     version="0.0.1",
     packages=["ha_renderer"],
     package_dir={"ha_renderer": "python_render"},
+    entry_points={
+        "console_scripts": ["ha_renderer=ha_renderer.app:main"],
+    },
     package_data={
         "ha_renderer": [
             "templates/*.j2",
