ab4e65f2d5867ae42c93e8ea6ccd1801931a4a76
root <stuart.lynne@gmail.com>
2026-01-18 12:34:59 -0800

    PUBLICIP

--- ha_renderer/entry/ha_renderer.sh
+++ ha_renderer/entry/ha_renderer.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+set -x
+PUBLICIP=$(ip route get 1 | sed -e 's/.*src //' -e 's/ .*//g')
+export PUBLICIP
 if [[ -f /entry/ha_renderer.env ]]; then
   # shellcheck disable=SC1091
   source /entry/ha_renderer.env
 fi
 
+echo PUBLIC_URL: ${PUBLIC_URL}
 
-exec /usr/local/bin/ha_renderer
+
+echo exec /usr/local/bin/ha_renderer
